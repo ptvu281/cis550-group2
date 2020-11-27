adeb868b321fd0b8c9a494436fd5a376995d2691
who-yahan <57080865+who-yahan@users.noreply.github.com>
2020-11-28 07:48:30 +0800

    Add client files via uploading

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import '../style/Dashboard.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PageNavbar from './PageNavbar';
+import GenreButton from './GenreButton';
+import DashboardMovieRow from './DashboardMovieRow';
+
+export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // The state maintained by this React Component. This component maintains the list of genres,
+    // and a list of movies for a specified genre.
+    this.state = {
+      genres: [],
+      movies: []
+    }
+
+    this.showMovies = this.showMovies.bind(this);
+  }
+
+  // React function that is called when the page load.
+  componentDidMount() {
+    // Send an HTTP request to the server.
+    fetch("http://localhost:8081/genres", {
+      method: 'GET' // The type of HTTP request.
+    })
+      .then(res => res.json()) // Convert the response data to a JSON.
+      .then(genreList => {
+        console.log(genreList)
+        if (!genreList) return;
+        // Map each genreObj in genreList to an HTML element:
+        // A button which triggers the showMovies function for each genre.
+        let genreDivs = genreList.map((genreObj, i) =>
+          <GenreButton id={"button-" + genreObj.genre} onClick={() => this.showMovies(genreObj.genre)} genre={genreObj.genre} />
+        );
+
+        // Set the state of the genres list to the value returned by the HTTP response from the server.
+        this.setState({
+          genres: genreDivs
+        })
+      })
+      .catch(err => console.log(err))	// Print the error if there is one.
+  }
+
+
+  /* ---- Q1b (Dashboard) ---- */
+  /* Set this.state.movies to a list of <DashboardMovieRow />'s. */
+  showMovies(genre) {
+    fetch("http://localhost:8081/genres/" + genre, {
+      method: 'GET' // The type of HTTP request.
+    })
+      .then(res => res.json()) // Convert the response data to a JSON.
+      .then(movieList => {
+        console.log(movieList)
+        if (!movieList) return;
+        // Map each genreObj in movieList to an HTML element:
+        // A button which triggers the showMovies function for each genre.
+        let movieDivs = movieList.map((movieObj, i) =>
+          <DashboardMovieRow key={i} title={movieObj.title} rating={movieObj.rating} voting={movieObj.vote_count}/>
+        );
+
+        // Set the state of the genres list to the value returned by the HTTP response from the server.
+        this.setState({
+          movies: movieDivs
+        })
+      })
+      .catch(err => console.log(err))	// Print the error if there is one.
+  }
+
+  render() {    
+    return (
+      <div className="Dashboard">
+
+        <PageNavbar active="dashboard" />
+
+        <br></br>
+        <div className="container movies-container">
+          <div className="jumbotron">
+            <div className="h5">Top Movies</div>
+            <div className="genres-container">
+              {this.state.genres}
+            </div>
+          </div>
+
+          <br></br>
+          <div className="jumbotron">
+            <div className="movies-container">
+              <div className="movies-header">
+                <div className="header-lg"><strong>Title</strong></div>
+                <div className="header"><strong>Rating</strong></div>
+                <div className="header"><strong>Vote Count</strong></div>
+              </div>
+              <div className="results-container" id="results">
+                {this.state.movies}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
