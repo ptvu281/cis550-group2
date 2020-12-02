edf71e5989ab93756b09c14b1d8e63cb050d622b
victorphun <victorphun@gmail.com>
2020-12-02 08:51:20 -0500

    minor typo fixes

--- server/db-config.js
+++ server/db-config.js
@@ -3,6 +3,6 @@ module.exports = {
   host: "cis550projdb.cg1k49oyid4p.us-east-1.rds.amazonaws.com",
   port: "1517",
   user: "admin",
-  password: "cis550group2!",
+  password: "cis550group2",
   database: "HealthInsuranceMarketplaceDB"
 };
--- server/routes.js
+++ server/routes.js
@@ -45,21 +45,32 @@ function getRecs(req, res) {
   var inputSmoking = req.params.smoking;
   var inputFamily = req.params.family;
   var query = `
-  SELECT Plan.PlanId, Benefits.BenefitName, Issuer.IssuerMarketplaceMarketingName, Network.NetworkName, Benefits.CopayOutofNet,
-  Benefits.CoinsOutofNet CASE WHEN ${inputSmoking} = “True”
-  THEN Rate.IndividualRate
-  ELSE Rate.IndividualTobaccoRate
-  END AS IndividualRate,
-  Couple, PrimarySubscriberAndOneDependent, PrimarySubscriberAndTwoDependents, PrimarySubscriberAndThreeOrMoreDependents, CoupleAndOneDependent, CoupleAndTwoDependents, CoupleAndThreeOrMoreDependents
-  FROM Plan JOIN Rate ON Plan.PlanId = Rate.PlanId
+  SELECT Plan.PlanId, Benefits.BenefitName, Benefits.CopayOutofNetAmount, Benefits.CoinsOutofNet, Rates.IndividualRate
+  FROM Plan JOIN Rates ON Plan.PlanId = Rates.PlanId
   JOIN Benefits ON Plan.PlanId = Benefits.PlanId
-  JOIN Issuer ON Plan.IssuerId = Issuer.IssuerId
-  JOIN Network ON Issuer.NetworkId = Network.NetworkId
-  JOIN FamilyOption ON FamilyOption.PlanId=Plan.PlanId
-  WHERE Plan.StateCode = ${inputLocation} AND
-  Rate.Age = ${inputAge} AND
-  IndividualRate IS NOT NULL AND
-  Plan.BusinessYear = 2016;`;
+  WHERE Plan.StateCode = "AL" AND
+  Rates.Age = "21 and over" AND
+  IndividualRate IS NOT NULL;`;
+  // var query = `
+  // SELECT Plan.PlanId, Benefits.BenefitName, Issuer.IssuerMarketplaceMarketingName, Network.NetworkName, Benefits.CopayOutofNet,
+  // Benefits.CoinsOutofNet, CASE WHEN ${inputSmoking} = "No Smoking"
+  //   THEN Rates.IndividualRate
+  //   ELSE Rates.IndividualTobaccoRate
+  //   END AS IndividualRate,
+  // CASE WHEN ${inputFamily} = "Couple" THEN Couple END AS GroupRate,
+  // CASE WHEN ${inputFamily} = "Primary Subscriber And One Dependent" THEN PrimarySubscriberAndOneDependent ELSE NULL END AS GroupRate,
+  // CASE WHEN ${inputFamily} = "Primary Subscriber And Two Dependents" THEN PrimarySubscriberAndTwoDependents ELSE NULL END AS GroupRate,
+  // CASE WHEN ${inputFamily} = "Primary Subscriber And Three Or More Dependents" THEN PrimarySubscriberAndThreeOrMoreDependents ELSE NULL END AS GroupRate,
+  // CASE WHEN ${inputFamily} = "Couple And One Dependent" THEN CoupleAndOneDependent ELSE NULL END AS GroupRate,
+  // FROM Plan JOIN Rates ON Plan.PlanId = Rates.PlanId
+  // JOIN Benefits ON Plan.PlanId = Benefits.PlanId
+  // JOIN Issuer ON Plan.IssuerId = Issuer.IssuerId
+  // JOIN Network ON Issuer.NetworkId = Network.NetworkId
+  // JOIN FamilyOption ON FamilyOption.PlanId=Plan.PlanId
+  // WHERE Plan.StateCode = ${inputLocation} AND
+  // Rates.Age = ${inputAge} AND
+  // IndividualRate IS NOT NULL ORDER BY Benefits.BenefitName;`;
+
   connection.query(query, function(err, rows, fields){
     if(err) console.log(err);
     else{
